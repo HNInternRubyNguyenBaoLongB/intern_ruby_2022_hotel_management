5cce604b5cf553b183744b0e1b255869b2aebb75
baolong187 <nguyen.bao.long-b@sun-asterisk.com>
2022-08-10 08:02:50 +0700

    search room

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,33 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 global.toastr = require('toastr')
+
+$(document).on('ready turbolinks:load', function(){
+  $(document).on('change', '.upload-image', function(){
+    var preview = $(this).siblings('.preview')[0];
+    var files   = $(this)[0].files;
+
+    function readAndPreview(file) {
+
+      if ( /\.(jpe?g|png|gif)$/i.test(file.name) ) {
+        var reader = new FileReader();
+
+        reader.addEventListener("load", function () {
+          var image = new Image();
+          image.height = 250;
+          image.width = 250;
+          image.title = file.name;
+          image.src = this.result;
+          preview.appendChild( image );
+        }, false);
+
+        reader.readAsDataURL(file);
+      }
+
+    }
+
+    if (files) {
+      [].forEach.call(files, readAndPreview);
+    }
+  })
+})
